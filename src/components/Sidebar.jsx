71f4d784f8d4d9cdb1accec9578a5098552cacce
amanbhartiiii <amankumarbharti824112@gmail.com>
2025-10-15 20:56:00 +0530

    Some changes in Sidebar UI

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,42 @@
 import { IoMdClose } from "react-icons/io";
+import { IoMdHome } from "react-icons/io";
+import { FaCircleQuestion } from "react-icons/fa6";
+import { MdLeaderboard } from "react-icons/md";
+import { FaBookOpen } from "react-icons/fa";
+import { TbHandFingerRight } from "react-icons/tb";
 
 const Sidebar = ({openSidebar, setOpenSidebar}) => {
   return (
-    <div className={`bg-black/70 backdrop-blur-md h-screen w-screen fixed top-0 z-50 pt-3 text-white transition-transform duration-300 ease-linear ${openSidebar ? "translate-x-0" : "-translate-x-full"}`}>
-      <div className="flex justify-between border-b p-2">
+    <div
+      className={`bg-black/70 backdrop-blur-md h-screen w-[300px] fixed top-0 z-50 pt-3 text-white transition-transform duration-300 ease-linear ${
+        openSidebar ? "translate-x-0" : "-translate-x-full"
+      }`}
+    >
+      <div className="flex justify-between border-b p-2 pl-4">
         <div className="text-xl">Aman Bharti</div>
-        <button className="text-2xl" onClick={()=>setOpenSidebar(!openSidebar)}>
+        <button
+          className="text-2xl"
+          onClick={() => setOpenSidebar(!openSidebar)}
+        >
           <IoMdClose />
         </button>
       </div>
-      <ul className="pl-4 pt-2 space-y-2">
-        <li className="cursor-pointer hover:text-slate-500">Home</li>
-        <li className="cursor-pointer hover:text-slate-500">Quiz</li>
-        <li className="cursor-pointer hover:text-slate-500">Leaderboard</li>
-        <li className="cursor-pointer hover:text-slate-500">Notes</li>
-        <li className="cursor-pointer hover:text-slate-500">About</li>
+      <ul className="pt-3 pl-6 space-y-4">
+        <li className="cursor-pointer hover:text-slate-500 flex items-center space-x-3">
+          <IoMdHome /> <p>Home</p>
+        </li>
+        <li className="cursor-pointer hover:text-slate-500 flex items-center space-x-3">
+          <FaCircleQuestion /> <p>Quiz</p>
+        </li>
+        <li className="cursor-pointer hover:text-slate-500 flex items-center space-x-3">
+          <MdLeaderboard /> <p>Leaderboard</p>
+        </li>
+        <li className="cursor-pointer hover:text-slate-500 flex items-center space-x-3">
+          <FaBookOpen /> <p>Notes</p>
+        </li>
+        <li className="cursor-pointer hover:text-slate-500 flex items-center space-x-3">
+          <TbHandFingerRight /> <p>About</p>
+        </li>
       </ul>
     </div>
   );
